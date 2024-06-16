fbf9ce7d89db43ad186e98798d6853742ada2835
mayank-yadav <mayankyadav1503@gmail.com>
2024-06-16 15:02:07 +0530

    Apache Kafka: Adding Producer and Consumer logic

---
+++ src/main/java/com/mayanktech/springboot/SpringbootKakaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.mayanktech.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKakaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKakaTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mayanktech/springboot/SpringbootKakaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayanktech.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKakaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
